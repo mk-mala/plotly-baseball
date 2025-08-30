a5033c3d9e63f931fb2f743fed55d3f2bbfe7f06
Miguel Malabayabas <mk_mala@Miguels-Air.attlocal.net>
2025-08-30 13:08:19 -0500

    Updating bubble chart.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,15 @@ bar_charts = st.Page("pages/bar_charts.py", title="Bar Charts")
 line_charts = st.Page("pages/line_charts.py", title="Line Charts")
 scatter_plots = st.Page("pages/scatter_plots.py", title="Scatter Plots")
 pie_charts = st.Page("pages/pie_charts.py", title="Pie Charts")
+bubble_charts = st.Page("pages/bubble_charts.py", title="Bubble Charts")
 
 pg = st.navigation([
     home_page,
     bar_charts,
     line_charts,
     scatter_plots,
-    pie_charts
+    pie_charts,
+    bubble_charts
 ])
 
 pg.run()
